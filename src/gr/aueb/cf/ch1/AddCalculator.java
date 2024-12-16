840f2299de069150bc66a419e9b799e99eae3c04
Andreas Krikis <andreaskrikiss@gmail.com>
2024-12-16 19:42:30 +0200

    Project created

---
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.ch1;
+
+public class AddCalculator {
+
+    public static void main(String[] args) {
+        int a= 10;
+        int b = 20;
+        int result = 0;
+
+        result = add(a, b);
+
+
+    }
+
+    public static int add(int a, int b) {
+        return a+ b;
+    }
+}
+
+
---
+++ src/gr/aueb/cf/ch2/SubCalculator.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch2;
+
+public class SubCalculator {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 2;
+        int result = 0;
+
+        result = sub(a, b);
+    }
+
+    public static int sub(int a, int b) {
+        return a- b;
+    }
+}
